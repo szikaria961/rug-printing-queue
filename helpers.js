ec9c510e76c2707f919300f6920e2a2db0693969
szikaria961 <szikaria961@gmail.com>
2021-02-13 21:01:30 -0500

    Add helpers file and setup test runner with Jest

---
+++ helpers.js
@@ -0,0 +1,10 @@
+const getNextItemsToPrint = ({
+  roll_length: rollLength,
+  include_rush: includeRush
+}) => {
+  return null;
+}
+
+module.exports = {
+  getNextItemsToPrint
+}
--- schema.js
+++ schema.js
@@ -1,10 +1,11 @@
 const { buildSchema } = require('graphql');
+const { getNextItemsToPrint } = require('./helpers');
 
 const schema = buildSchema(`
   scalar Date
 
   type Query {
-    getNextItemsToPrint(roll_length: Float!, include_rush: Boolean!): [Queue]
+    getNextItemsToPrint(roll_length: Float!, include_rush: Boolean!): Queue
   }
 
   type Queue {
@@ -24,53 +25,8 @@ const schema = buildSchema(`
 `);
 
 const rootValue = {
-  getNextItemsToPrint: ({
-    roll_length: rollLength,
-    include_rush: includeRush
-  }) => {
-    console.log({ rollLength, includeRush });
-
-    return [
-      {
-        "roll_id": 2562,
-        "length": 14.2,
-        "plan": [
-          {
-            "id": 5683,
-            "position": 1,
-            "size": "2.5x7",
-            "order_date": "2020-10-13 04:27:30-07:00",
-            "sku": "RS-1234-27",
-            "rush": true
-          },
-          {
-            "id": 2562,
-            "position": 1,
-            "size": "2.5x7",
-            "order_date":"2020-09-14 16:24:24-07:00",
-            "sku": "RC-1013-27",
-            "rush": false
-          },
-          {
-            "id": 9876,
-            "position": 2,
-            "size": "3x5",
-            "order_date":"2020-11-22 10:02:06-07:00",
-            "sku": "RS-1234-27",
-            "rush": true
-          },
-          {
-            "id": 5684,
-            "position": 3,
-            "size": "3x5",
-            "order_date":"2020-11-22 10:30:24-07:00",
-            "rush": true
-          }
-        ]
-      }
-    ]
-  }
-};
+  getNextItemsToPrint
+}
 
 module.exports = {
   schema,
